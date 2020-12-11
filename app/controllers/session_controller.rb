632a54bb3b949ede80c065c1b475c31d2d6f5d83
Sean <Sransonette@gmail.com>
2020-12-10 23:28:10 -0600

    Done.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,11 @@
 class SessionController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(:name => params[:user][:name])
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:user_password])
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'users/new'
   get 'users/create'
   get 'users/show'
