4243e57aa98d9ab11aa65cdcba5e5468f3af0ce7
jillbowen <jillian.b.short@gmail.com>
2020-12-14 22:57:56 -0800

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+    skip_before_action :verify_user, only: [:home, :new, :create]
+    
+    def home
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        if (@user = User.create(user_params))
+            if @user.save
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            end
+        else
+            render 'new'
+        end
+      end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        # if !current_user.admin
+        #     if current_user != @user
+        #         redirect_to root_path
+        #     end
+        # end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(
+            :name, 
+            :height, 
+            :happiness, 
+            :nausea, 
+            :tickets,
+            :password,
+            :admin
+        )
+    end
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,24 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction
+    belongs_to :user
+
+    def take_ride
+        user_ticket_count = self.user.tickets - self.attraction.tickets
+        user_nausea = self.user.nausea + self.attraction.nausea_rating
+        user_happiness = self.user.happiness + self.attraction.happiness_rating
+
+        self.user.update(
+            :tickets => user_ticket_count,
+            :nausea => user_nausea,
+            :happiness => user_happiness
+        )
+        
+        if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+        elsif self.user.tickets < self.attraction.tickets
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        else self.user.height < self.attraction.min_height
+            "Sorry. You are not tall enough to ride the Roller Coaster."
+        end
+    end
 end
