efff544c34212d57298430a9fcf9f33c5dbcbc23
Justin Lupica <justinlupica@gmail.com>
2020-12-31 14:46:35 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+        @users = User.all
+      end
+    
+      def create
+        @user = User.find_by(id: params[:user_name])
+        if @user
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
+      end
+    
+      def destroy
+        # binding.pry
+        session[:user_id] = nil
+    
+        redirect_to root_path
+      end
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,23 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction
+    belongs_to :user
+
+    def take_ride
+        result = ["Sorry."]
+        if attraction.tickets.to_i > user.tickets.to_i.to_i
+          result << "You do not have enough tickets to ride the #{attraction.name}."
+        end
+        if attraction.min_height.to_i > user.height.to_i
+          result << "You are not tall enough to ride the #{attraction.name}."
+        end
+        if result.count > 1
+          result.join(' ')
+        else
+          user.tickets -= attraction.tickets.to_i
+          user.nausea += attraction.nausea_rating.to_i
+          user.happiness += attraction.happiness_rating.to_i
+          user.save
+          "Thanks for riding the #{attraction.name}!"
+        end
+    end
 end
