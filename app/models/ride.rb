195eaa4c5e48b1b40a1a201f86ce1900a82e83a3
Veenie <raveen08@gmail.com>
2021-01-04 14:55:27 -0800

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,21 +3,47 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-    msg = []
-    if user.tickets < attraction.tickets
-      msg << "Sorry. You do not have enough tickets the #{self.attraction.name}."
+    user_has_enough_tickets, user_is_tall_enough = user_meets_requirements
+    if user_has_enough_tickets && user_is_tall_enough
+      start_ride
+    elsif user_is_tall_enough && !user_has_enough_tickets
+      "Sorry. " + not_enough_tickets
+    elsif !user_is_tall_enough && user_has_enough_tickets
+      "Sorry. " + not_tall_enough
+    elsif !user_is_tall_enough && !user_has_enough_tickets
+      "Sorry. " + not_enough_tickets + " " + not_tall_enough
     end
-    if user.height < attraction.min_height
-      msg << "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+  end
+
+  def user_meets_requirements
+    user_has_enough_tickets, user_is_tall_enough = false
+    if self.user.tickets >= self.attraction.tickets
+      user_has_enough_tickets = true
     end
-    if user.tickets >= attraction.tickets && user.height >= attraction.min_height
-      user.tickets -= attraction.tickets
-      user.nausea += attraction.nausea_rating
-      user.happiness += attraction.happiness_rating
-      user.save
-      msg << "Thanks for riding the #{self.attraction.name}!"
+    if self.user.height >= self.attraction.min_height
+      user_is_tall_enough = true
     end
-    msg.join(" ")
+    return [user_has_enough_tickets, user_is_tall_enough]
+  end
+
+  def start_ride
+    new_happiness = self.user.happiness + self.attraction.happiness_rating
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
+    new_ticket_count = self.user.tickets - self.attraction.tickets
+    self.user.update(
+      :happiness => new_happiness,
+      :nausea => new_nausea,
+      :tickets => new_ticket_count
+    )
+    "Thanks for riding the #{self.attraction.name}!"
+  end
+
+  def not_enough_tickets
+    "You do not have enough tickets to ride the #{self.attraction.name}."
+  end
+
+  def not_tall_enough
+    "You are not tall enough to ride the #{self.attraction.name}."
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
 
+
   def mood
-    if self.happiness > self.nausea
-      return 'happy'
-    else
-      return 'sad'
+    unless admin
+      happiness > nausea ? "happy" : "sad"
     end
   end
 
