dcb8440ed14f9a3d47627c1e1d604b297b43308e
Raveen K Sharma <raveen08@gmail.com>
2021-01-04 21:39:39 +0000

    Done.

--- db/migrate/20210104203159_create_users.rb
+++ db/migrate/20210104203159_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :password_digest
       t.integer :nausea, default: 0
       t.integer :happiness, default: 0
       t.integer :tickets, default: 0
