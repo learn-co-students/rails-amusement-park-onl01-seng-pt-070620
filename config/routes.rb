802739c785cff93ea1ea710d41806f5d6c9f03ba
Raveen K Sharma <raveen08@gmail.com>
2021-01-04 21:00:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :attractions
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/users/new' => 'users#new'
+  post '/logout' => 'sessions#destroy'
+  root 'welcome#home'
+  post '/take_ride' => 'users#ride'
 end
